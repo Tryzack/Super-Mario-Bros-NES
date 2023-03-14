b6275f325c01e05217c9d34de5be31adb4199a22
Tryzack <44532224+Tryzack@users.noreply.github.com>
2023-03-14 19:35:32 -0400

    Delete Mario/Mario directory

--- Mario/Mario/Scenes/moneda_bonus.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite2D
-
-func animate(): #animar moneda
-	get_node("AnimationPlayer").play("idle")
-
-func _on_animation_player_animation_finished(_anim_name):
-	queue_free() #eliminar moneda
